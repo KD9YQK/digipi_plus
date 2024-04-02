f953da1a771216566cd167040b03b3154b8876fe
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-02 12:56:22 -0500

    Add files via upload

---
+++ home/games/gofish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export HOME="/home/pi"
+export TERM="linux"
+cd /home/pi/games
+#/usr/games/frotz /home/pi/zork/DATA/ZORK1.DAT
+echo "Go Fish"
+echo "Instructions?"
+/usr/games/go-fish
+exit 0
