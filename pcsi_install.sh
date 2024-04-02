cc930b2fcc483bbc1b2f552557eb5f05a5159bbf
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-02 13:04:17 -0500

    Update pcsi_install.sh

--- pcsi_install.sh
+++ pcsi_install.sh
@@ -9,6 +9,8 @@ sudo cp index.new.php /var/www/html/index.php
 sudo rm index.new.php
 sudo mkdir /var/www/html/pcsi
 sudo cp www/pcsi/index.php /var/www/html/pcsi/
+cp home/pcsi.sh ~/
+chmod +x ~/pcsi.sh
 sudo cp ~/direwolf.tnc.conf ~/direwolf.pcsi.conf
 sudo cp services/pcsi.service /etc/systemd/system/
 sudo systemctl daemon-reload
