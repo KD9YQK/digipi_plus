8810cc356d2c888838232c56910d5b961fdb7448
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-03 14:03:40 -0500

    Update pcsi_setup.py

--- pcsi_setup.py
+++ pcsi_setup.py
@@ -1,3 +1,21 @@
+import os
+
+
+path = "/etc/systemd/system"
+no_change = ['autohotspot.service', 'digipi-boot.service', 'digipi-resolv.service', 'rigctld.service', 'webchat.service',  'gpio_redled.service', 'chronyd.service', 'dbus-fi.w1.wpa_supplicant1.service', 
+             'dbus-org.bluez.service', 'dbus-org.freedesktop.Avahi.service', 'dbus-org.freedesktop.nm-dispatcher.service', 'dbus-org.freedesktop.timesync1.service' ]
+
+for x in os.listdir(path):
+    if x.endswith(".service"):
+        if x in no_change:
+            pass
+        else:
+            with open(f"{path}/{x}") as f_old, open(f"services/{x}", "w") as f_new:
+                for line in f_old:
+                    if 'ExecStartPre=+systemctl stop' in line:
+                        line = line.replace('stop', 'stop pcsi')
+                    f_new.write(line)
+
 
 with open("/var/www/html/index.php") as f_old, open("index.new.php", "w") as f_new:
     script_count = 0
