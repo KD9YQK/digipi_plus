12722a47f4a1b69192f419e12262324aa747b650
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-03 20:20:06 -0500

    Add files via upload

---
+++ www/plus_form.php
@@ -0,0 +1,16 @@
+<br/>
+<br/>
+<h3>DigiPi Plus</h3>
+<table width="400px">
+ <col width="10px" />
+ <col width="320px" />
+ <col width="70" />
+<?php
+#-- Plus Buttons
+
+#-- Plus Sigkill
+
+?>
+</table>
+<br/>
+<br/>
