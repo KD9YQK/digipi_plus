98d0fad111dbcd8ca09b6f4bf65d0016e38215e1
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-03 21:20:25 -0500

    Update pcsi_install.sh

--- pcsi_install.sh
+++ pcsi_install.sh
@@ -4,9 +4,8 @@ echo "Backing up /var/www/html/index.php as /var/www/html/index.php.bak"
 sudo cp /var/www/html/index.php /var/www/html/index.php.bak
 
 python3 pcsi_setup.py
-sudo rm /var/www/html/index.php
-sudo cp index.new.php /var/www/html/index.php
-sudo rm index.new.php
+sudo cp temp/*.php /var/www/html/
+sudo rm temp/*.php
 sudo mkdir /var/www/html/pcsi
 sudo cp www/pcsi/index.php /var/www/html/pcsi/
 cp home/pcsi.sh ~/
