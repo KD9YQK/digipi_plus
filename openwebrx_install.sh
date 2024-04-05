e1d831fbcfceaa3b300ada76908391c1fb17fdc2
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-04 23:31:23 -0500

    Update openwebrx_install.sh

--- openwebrx_install.sh
+++ openwebrx_install.sh
@@ -1,5 +1,4 @@
-sudo wget -O /usr/share/keyrings/openwebrx.gpg https://repo.openwebrx.de/openwebrx.gpg
-sudo echo "deb [signed-by=/usr/share/keyrings/openwebrx.gpg] https://repo.openwebrx.de/debian/ experimental main" > temp/openwebrx-experimental.list
-sudo mv temp/openwebrx-experimental.list /etc/apt/sources.list.d/openwebrx-experimental.list
+sudo wget -O - https://luarvique.github.io/ppa/openwebrx-plus.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/openwebrx-plus.gpg
+sudo echo "deb [signed-by=/etc/apt/trusted.gpg.d/openwebrx-plus.gpg] https://luarvique.github.io/ppa/bookworm ./" > /etc/apt/sources.list.d/openwebrx-plus.list
 sudo apt-get update
 sudo apt-get install openwebrx
