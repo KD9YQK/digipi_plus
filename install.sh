8092fb0f6e23a02f9a95d0d4f95e12f206919b61
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-04 21:33:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@ echo ""
 echo "DigiPi Plus Installer"
 echo "Created by Bob - KD9YQK"
 echo ""
-
-echo "Checking for Base..." -n
+sleep 1
+echo -n "Checking for Base..."
+sleep 1
 if [ ! -f plus.base ]; then
     echo "NOT FOUND"
     echo "Installing DigiPi Plus Base"
+    sleep 1
     echo "Backing up files"
     cp /var/www/html/index.php backup/ -v
     cp ~/.emwmrc backup/emwmrc -v
@@ -21,10 +23,12 @@ else
     echo "OK"
 fi
 
-echo "Checking for AX25 Upgrade..." -n
+echo -n "Checking for AX25 Upgrade..."
+sleep 1
 if [ ! -f plus.node ]; then
     echo "NOT FOUND"
     echo "Installing AX25 Node Upgrade"
+    sleep 1
     bash node_upgrade.sh
     touch plus.node
     echo "AX25 Upgrade Installed"
@@ -32,10 +36,12 @@ else
     echo "OK"
 fi
 
-echo "Checking for PCSI..." -n
+echo -n "Checking for PCSI..."
+sleep 1
 if [ ! -f plus.pcsi ]; then
     echo "NOT FOUND"
     echo "Installing PCSI"
+    sleep 1
     bash pcsi_install.sh
     touch plus.pcsi
     echo "PCSI Installed"
@@ -43,10 +49,12 @@ else
     echo "OK"
 fi
 
-echo "Checking for RTL-SDR Drivers..." -n
+echo -n "Checking for RTL-SDR Drivers..."
+sleep 1
 if [ ! -f plus.rtl-sdr ]; then
     echo "NOT FOUND"
     echo "Installing RTL-SDR Drivers"
+    sleep 1
     sudo apt install rtl-sdr -y
     echo "Installing udev rules"
     sudo cp rtl-sdr/10-rtl-sdr.rules /etc/udev/rules.d/ -v
@@ -57,10 +65,12 @@ else
     echo "OK"
 fi
 
-echo "Checking for RTL-SDR iGate..." -n
+echo -n "Checking for RTL-SDR iGate..."
+sleep 1
 if [ ! -f plus.sdr_igate ]; then
     echo "NOT FOUND"
     echo "Installing RTL-SDR iGate"
+    sleep 1
 
     touch plus.sdr_igate
     echo "RTL-SDR iGate Installed"
