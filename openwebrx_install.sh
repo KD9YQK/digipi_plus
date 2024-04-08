eba610d53d590ca69125b8e2b907fc00833f1c9d
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-07 19:12:58 -0500

    Update openwebrx_install.sh

--- openwebrx_install.sh
+++ openwebrx_install.sh
@@ -2,3 +2,6 @@ sudo wget -O - https://luarvique.github.io/ppa/openwebrx-plus.gpg | sudo gpg --d
 sudo echo "deb [signed-by=/etc/apt/trusted.gpg.d/openwebrx-plus.gpg] https://luarvique.github.io/ppa/bookworm ./" > /etc/apt/sources.list.d/openwebrx-plus.list
 sudo apt-get update
 sudo apt-get install openwebrx
+sudo systemctl disable openwebrx
+sudo systemctl stop openwebrx
+python3 openwebrx_helpyer.py
