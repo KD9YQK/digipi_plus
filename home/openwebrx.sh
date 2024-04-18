7d8bfa9fe4a9ca73607e291abae8ba52c73d338d
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-18 00:21:30 -0500

    Update openwebrx.sh

--- home/openwebrx.sh
+++ home/openwebrx.sh
@@ -14,12 +14,7 @@ sudo cp -a /home/pi/config/openwebrx/* /run/openwebrx/
 sudo chown -R pi:pi /run/openwebrx
 openwebrx -c ~/openwebrx.conf &
 
-# remove "-o" flag to see list of stations
-if [ $NEWLAT = 39.9999 ]; then  # location not yet set
-   sudo /home/pi/direwatch.py -o --log "/run/direwolf.log" --title_text "DigiPi OpenWebRX"  &
-else
-   sudo /home/pi/direwatch.py -o --log "/run/direwolf.log" --title_text "DigiPi OpenWebRX" --lat $NEWLAT --lon $NEWLON  &
-fi
+sudo /home/pi/direwatch.py -o --log "/run/direwolf.log" --title_text "DigiPi OpenWebRX"  &
 
 
 sleep infinity
