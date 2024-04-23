65f59f81569d3e6b06da2fde55f8a2d6dc4d9a7d
HansKillinger <hanskillinger@protonmail.com>
2024-04-23 18:14:25 -0500

    fixes

--- www/index_plus.php
+++ www/index_plus.php
@@ -3,7 +3,7 @@
 <form action="index_plus.php" method="post">
 <table>
 <tr>
-    <td>
+    <td width=400>
         <div class="tab" style="text-align: center;">
             <center>
             <button class="tablinks" type="button" id="defaultOpen" onclick="openMenu(event, 'Services')">Services</button>
