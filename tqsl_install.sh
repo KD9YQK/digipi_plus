aa2222907d1e6e1745034ec10368a093490ae4af
HansKillinger <hanskillinger@protonmail.com>
2024-05-02 13:08:17 -0500

    bug fix

--- tqsl_install.sh
+++ tqsl_install.sh
@@ -1,7 +1,7 @@
 sudo apt install cmake libssl-dev libsqlite3-dev libcurl4-openssl-dev libwxgtk3.0-gtk3-dev -y
 wget http://www.arrl.org/files/file/LoTW%20Instructions/tqsl-2.7.3.tar.gz
 tar -zxvf tqsl-2.7.3.tar.gz
-cd tqsl-2.7.2
+cd tqsl-2.7.3
 cmake .
 make
 sudo make install
