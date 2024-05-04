527001a3ea8d2f0adc2a5aaeae06effff8313a5d
HansKillinger <hanskillinger@protonmail.com>
2024-05-03 20:59:36 -0500

    New installer

--- digipi_plus.sh
+++ digipi_plus.sh
@@ -96,9 +96,6 @@ else
     options+=(11 "RTL-SDR iGate" on)
 fi
 
-clear
-sleep 1
-clear
 
 #build dialogue box with menu options
 cmd=(dialog --backtitle "DigiPi Plus" --checklist "Pick 1 or more options" 22 50 16)
@@ -107,16 +104,176 @@ choices=($("${cmd[@]}" "${options[@]}" 2>&1 1>/dev/tty))
 for choice in "${choices[@]}"; do
     case $choice in
         1)
-            echo "First Option"
+            echo -n "Checking for AX25 Upgrade..."
+            sleep 1
+            if [ ! -f saves/plus.node ]; then
+                echo "NOT FOUND"
+                echo "Installing AX25 Node Upgrade"
+                sleep 1
+                bash node_upgrade.sh
+                touch saves/plus.node
+                echo "AX25 Upgrade Installed";;
+            else
+                echo "OK"
+            fi
             ;;
         2)
-            echo "Second Option"
+            echo -n "Checking for UHF Upgrade..."
+            sleep 1
+            if [ ! -f saves/plus.uhf ]; then
+                echo "NOT FOUND"
+                echo "Installing UHF TNC and Node"
+                sleep 1
+                bash uhf_upgrade.sh
+                touch saves/plus.uhf
+                echo "UHF Upgrade Installed";;
+            else
+                echo "OK"
+            fi
             ;;
         3)
-            echo "Third Option"
+            echo -n "Checking for PCSI..."
+            sleep 1
+            if [ ! -f saves/plus.pcsi ]; then
+                echo "NOT FOUND"
+                echo "Installing PCSI"
+                sleep 1
+                bash pcsi_install.sh
+                touch saves/plus.pcsi
+                echo "PCSI Installed";;
+            else
+                echo "OK"
+            fi
             ;;
         4)
-            echo "Fourth Option"
+            echo -n "Checking for Grid Tracker..."
+            sleep 1
+            if [ ! -f saves/plus.gridtracker ]; then
+                echo "NOT FOUND"
+                echo "Installing Grid Tracker"
+                mkdir ~/Documents
+                sleep 1
+                curl  https://debian.gridtracker.org/gridtracker_deb_install.sh | sudo bash
+                sudo apt update
+                sudo apt install gridtracker -y
+                sudo sed -i '/DigiPi Plus/a \        "Grid Tracker"               f.exec "gridtracker &"' /home/pi/.emwmrc
+                touch saves/plus.gridtracker
+                echo "Grid Tracker Installed";;
+            else
+                echo "OK"
+            fi
             ;;
+        5)
+            echo -n "Checking for JS8Spotter..."
+            sleep 1
+            if [ ! -f saves/plus.js8spotter ]; then
+                echo "NOT FOUND"
+                echo "Installing JS8Spotter"
+                sleep 1
+                wget https://kf7mix.com/files/js8spotter/js8spotter-110b_src.zip
+                mkdir js8spotter
+                unzip js8spotter-110b_src.zip
+                cp -r js8spotter-110b_src/* js8spotter/
+                rm -r js8spotter-110b_src
+                rm js8spotter-110b_src.zip
+                sudo sed -i '/DigiPi Plus/a \        "JS8Spotter"               f.exec "cd /home/pi/digipi_plus/js8spotter && python3 js8spotter.py &"' /home/pi/.emwmrc
+                touch saves/plus.js8spotter
+                echo "JS8Spotter Installed";;
+            else
+                echo "OK"
+            fi
+            ;;
+        6)
+            echo -n "Checking for gPredict..."
+            sleep 1
+            if [ ! -f saves/plus.gpredict ]; then
+                echo "NOT FOUND"
+                echo "Installing gPredict"
+                sleep 1
+                sudo apt install gpredict -y
+                sudo sed -i '/DigiPi Plus/a \        "gPredict"               f.exec "gpredict &"' /home/pi/.emwmrc
+                touch saves/plus.gpredict
+                echo "gPredict Installed";;
+            else
+                echo "OK"
+            fi
+            ;;
+        7)
+            echo -n "Checking for Xastir..."
+            sleep 1
+            if [ ! -f saves/plus.xastir ]; then
+                echo "NOT FOUND"
+                echo "Installing Xastir"
+                sleep 1
+                bash xastir_install.sh
+                touch saves/plus.xastir
+                echo "Xastir Installed";;
+            else
+                echo "OK"
+            fi
+            ;;
+        8)
+            echo -n "Checking for Trusted QSL..."
+            sleep 1
+            if [ ! -f saves/plus.tqsl ]; then
+                echo "NOT FOUND"
+                echo "Installing Trusted QSL"
+                sleep 1
+                bash tqsl_install.sh
+                touch saves/plus.tqsl
+                echo "Trusted QSL Installed";;
+            else
+                echo "OK"
+            fi
+        9)
+            echo -n "Checking for OpenWebRX..."
+            sleep 1
+            if [ ! -f saves/plus.openwebrx ]; then
+                echo "NOT FOUND"
+                echo "Installing OpenWebRX"
+                sleep 1
+                bash openwebrx_install.sh
+                touch saves/plus.openwebrx
+                echo "OpenWebRX Installed";;
+            else
+                echo "OK"
+            fi
+            ;;
+        10)
+            echo -n "Checking for RTL-SDR Drivers..."
+            sleep 1
+            if [ ! -f saves/plus.rtl-sdr ]; then
+                echo "NOT FOUND"
+                echo "Installing RTL-SDR Drivers"
+                sleep 1
+                sudo apt install rtl-sdr -y
+                echo "Installing udev rules"
+                sudo cp rtl-sdr/10-rtl-sdr.rules /etc/udev/rules.d/ -v
+                sudo udevadm control --reload-rules
+                touch saves/plus.rtl-sdr
+                echo "RTL-SDR Driver Installed";;
+            else
+                echo "OK"
+            fi
+            ;;
+        11)
+            echo -n "Checking for RTL-SDR iGate..."
+            sleep 1
+            if [ ! -f saves/plus.sdr_igate ]; then
+                echo "NOT FOUND"
+                echo "Installing RTL-SDR iGate"
+                sleep 1
+                chmod +x launchers/direwolf.rtlsdr.sh
+                python3 rtlsdr_helper.py
+                sudo mv temp/*.php www/ -v
+                sudo cp services/rtlsdr-igate.service /etc/systemd/system/ -v
+                sudo systemctl daemon-reload
+                touch saves/plus.sdr_igate
+                echo "RTL-SDR iGate Installed";;
+            else
+                echo "OK"
+            fi
+            ;;
+        
     esac
 done
