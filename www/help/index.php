1cacfd5198ba8e8be71fcb5afca0c9015af5008a
HansKillinger <hanskillinger@protonmail.com>
2024-06-01 15:48:39 -0500

    updates

--- www/help/index.php
+++ www/help/index.php
@@ -1,31 +1,48 @@
 <h1>DigiPi Plus</h1>
+<p>
+    DigiPi Plus is intended to be a suppliment to the DigiPi image created by Craig KM6LYW, with the goal being to add more functionality to the original project.
+    The idea is to be as least intrusive to the original system as possible, as well as provide a user friendly experience with minimal configuration effort.
+    Please enjoy the additions, and feel free to reach out with any comments, bugs, and/or suggestions.
 <br>
+- Bob KD9YQK
+<br><br>
+<h2>Upgrading DigiPi Plus</h2>
+<p>
+    DigiPi Plus is an ever-expanding project and will be subject to many potential updates in the future. To check for updates, or install additional software, run the following commands.
+<br>TODO
+
+<br><br>
 <h2>Trusted QSL</h2>
 <p>
     Trusted QSL can be opened by minimizing the active app in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
-<br>
+<br><br>
+
 <h2>OpenWebRX+</h2>
 <p>
     In order to save OpenWebRX+ configuration, first press the "Save Configs" button in the Settings tab, then turn OFF the OpenWebRX service.
     Next time you start, the configurations should be saved.
-<br>
+<br><br>
+
 <h2>Grid Tracker</h2>
 <p>
     Grid Tracker can be opened from within the VNC by minimizing WSJT-X and then right-clicking on the grey background. A menu should appear showing Grid Tracker.
     Pressing the "Save Configs" button in the Settings tab is required on your FIRST run in order to create the necessary files, or an error will occur.
-<br>
+<br><br>
+
 <h2>Xastir</h2>
 <p>
     On the FIRST initial run, press the "Save Configs" button in the Settings tab. Xastir will need to be manually started by right-clicking the grey background 
     in the VNC and selecting it from the menu. Make all your changes to the configuration at this time.
-<br>
+<br><br>
+
 <h2>JS8Spotter</h2>
 <p>
     JS8Spotter can be opened by minimizing JS8Call in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
 <br><br>
+
 <h2>Misc. Tutorials</h2>
 <p>
     Here are some useful tutorials to aide in the setup of DigiPi.
-<br>
+<p>
 <a href="extend_fs.php">Extend Filesystem</a> - Extend the filesystem on the SD-card.
 <br>
