ef97fb4b14682df18d34fd006fa98b49c0fc8662
HansKillinger <hanskillinger@protonmail.com>
2024-06-01 15:37:25 -0500

    Added to help page

--- www/help/index.php
+++ www/help/index.php
@@ -1,5 +1,9 @@
 <h1>DigiPi Plus</h1>
 <br>
+<h2>Trusted QSL</h2>
+<p>
+    Trusted QSL can be opened by minimizing the active app in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
+<br>
 <h2>OpenWebRX+</h2>
 <p>
     In order to save OpenWebRX+ configuration, first press the "Save Configs" button in the Settings tab, then turn OFF the OpenWebRX service.
@@ -15,5 +19,13 @@
     On the FIRST initial run, press the "Save Configs" button in the Settings tab. Xastir will need to be manually started by right-clicking the grey background 
     in the VNC and selecting it from the menu. Make all your changes to the configuration at this time.
 <br>
+<h2>JS8Spotter</h2>
+<p>
+    JS8Spotter can be opened by minimizing JS8Call in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
+<br><br>
 <h2>Misc. Tutorials</h2>
+<p>
+    Here are some useful tutorials to aide in the setup of DigiPi.
+<br>
 <a href="extend_fs.php">Extend Filesystem</a> - Extend the filesystem on the SD-card.
+<br>
