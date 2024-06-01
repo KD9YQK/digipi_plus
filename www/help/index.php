82ff5d41e2549a96ed6c60cf5fc9bb612a249088
HansKillinger <hanskillinger@protonmail.com>
2024-06-01 17:35:52 -0500

    minor changes to formatting

--- www/help/index.php
+++ www/help/index.php
@@ -3,55 +3,46 @@
 <div style="text-align: center;">
 <h1><u>DigiPi Plus</u></h1>
 </div>
-<p>
-    DigiPi Plus is intended to be a suppliment to the DigiPi image created by <a href="digipi.org">Craig KM6LYW</a>, with the goal being to add more functionality to the original project.
+<p>DigiPi Plus is intended to be a suppliment to the DigiPi image created by <a href="digipi.org">Craig KM6LYW</a>, with the goal being to add more functionality to the original project.
     The idea is to be minimally intrusive to the original system, yet provide a user friendly experience with minimal configuration effort.
     Please enjoy the additions, and feel free to reach out with any comments, bugs, and/or suggestions.
-<p>
-- Bob KD9YQK
+<p>- Bob KD9YQK
 <br><br>
 <h2><u>Upgrading DigiPi Plus</u></h2>
-<p>
-    DigiPi Plus is an ever-expanding project and will be subject to many potential updates in the future. To check for updates, or install additional software, run the following commands.
+<p>DigiPi Plus is an ever-expanding project and will be subject to many potential updates in the future. To check for updates, or install additional software, run the following commands.
 <pre><code>
     sudo remount
     cd digipi_plus
     bash digipi_plus.sh
 </code>
+<br>
 
-<br><br>
 <h2><u>Trusted QSL</u></h2>
-<p>
-    Trusted QSL can be opened by minimizing the active app in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
+<p>Trusted QSL can be opened by minimizing the active app in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
 <br><br>
 
 <h2><u>OpenWebRX+</u></h2>
-<p>
-    In order to save OpenWebRX+ configuration, first press the "Save Configs" button in the Settings tab, then turn OFF the OpenWebRX service.
+<p>In order to save OpenWebRX+ configuration, first press the "Save Configs" button in the Settings tab, then turn OFF the OpenWebRX service.
     Next time you start, the configurations should be saved.
 <br><br>
 
 <h2><u>Grid Tracker</u></h2>
-<p>
-    Grid Tracker can be opened from within the VNC by minimizing WSJT-X and then right-clicking on the grey background. A menu should appear showing Grid Tracker.
+<p>Grid Tracker can be opened from within the VNC by minimizing WSJT-X and then right-clicking on the grey background. A menu should appear showing Grid Tracker.
     Pressing the "Save Configs" button in the Settings tab is required on your FIRST run in order to create the necessary files, or an error will occur.
 <br><br>
 
 <h2><u>Xastir</u></h2>
-<p>
-    On the FIRST initial run, press the "Save Configs" button in the Settings tab. Xastir will need to be manually started by right-clicking the grey background 
+<p>On the FIRST initial run, press the "Save Configs" button in the Settings tab. Xastir will need to be manually started by right-clicking the grey background 
     in the VNC and selecting it from the menu. Make all your changes to the configuration at this time.
 <br><br>
 
 <h2><u>JS8Spotter</u></h2>
-<p>
-    JS8Spotter can be opened by minimizing JS8Call in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
+<p>JS8Spotter can be opened by minimizing JS8Call in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
 <br><br>
 
 <h2><u>Misc. Tutorials</u></h2>
-<p>
-    Here are some useful tutorials to aide in the setup of DigiPi.
-<p>
+<p>Here are some useful tutorials to aide in the setup of DigiPi.
+<br>
 <a href="extend_fs.php">Extend Filesystem</a> - Extend the filesystem on the SD-card.
 <br>
 </body>
