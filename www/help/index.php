408f0fd20696006d03d7c1e1bf13e922f256c2f5
HansKillinger <hanskillinger@protonmail.com>
2024-06-01 15:53:06 -0500

    added some underlines

--- www/help/index.php
+++ www/help/index.php
@@ -12,35 +12,35 @@
 <p>TODO
 
 <br><br>
-<h2>Trusted QSL</h2>
+<h2><u>Trusted QSL</u></h2>
 <p>
     Trusted QSL can be opened by minimizing the active app in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
 <br><br>
 
-<h2>OpenWebRX+</h2>
+<h2><u>OpenWebRX+</u></h2>
 <p>
     In order to save OpenWebRX+ configuration, first press the "Save Configs" button in the Settings tab, then turn OFF the OpenWebRX service.
     Next time you start, the configurations should be saved.
 <br><br>
 
-<h2>Grid Tracker</h2>
+<h2><u>Grid Tracker</u></h2>
 <p>
     Grid Tracker can be opened from within the VNC by minimizing WSJT-X and then right-clicking on the grey background. A menu should appear showing Grid Tracker.
     Pressing the "Save Configs" button in the Settings tab is required on your FIRST run in order to create the necessary files, or an error will occur.
 <br><br>
 
-<h2>Xastir</h2>
+<h2><u>Xastir</u></h2>
 <p>
     On the FIRST initial run, press the "Save Configs" button in the Settings tab. Xastir will need to be manually started by right-clicking the grey background 
     in the VNC and selecting it from the menu. Make all your changes to the configuration at this time.
 <br><br>
 
-<h2>JS8Spotter</h2>
+<h2><u>JS8Spotter</u></h2>
 <p>
     JS8Spotter can be opened by minimizing JS8Call in the VNC, right-clicking the grey background and selecting it from the pop-up menu.
 <br><br>
 
-<h2>Misc. Tutorials</h2>
+<h2><u>Misc. Tutorials</u></h2>
 <p>
     Here are some useful tutorials to aide in the setup of DigiPi.
 <p>
