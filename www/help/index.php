d81c5f7f4104b1566c01c418468f9e8fa09c67cd
HansKillinger <hanskillinger@protonmail.com>
2024-06-01 15:27:00 -0500

    Added more info to help pages

--- www/help/index.php
+++ www/help/index.php
@@ -1,4 +1,19 @@
 <h1>DigiPi Plus</h1>
 <br>
-<h2>Tutorials</h2>
-<a href="extend_fs.php">Extend Filesystem</a>
+<h2>OpenWebRX+</h2>
+<p>
+    In order to save OpenWebRX+ configuration, first press the "Save Configs" button in the Settings tab, then turn OFF the OpenWebRX service.
+    Next time you start, the configurations should be saved.
+<br>
+<h2>Grid Tracker</h2>
+<p>
+    Grid Tracker can be opened from within the VNC by minimizing WSJT-X and then right-clicking on the grey background. A menu should appear showing Grid Tracker.
+    Pressing the "Save Configs" button in the Settings tab is required on your FIRST run in order to create the necessary files, or an error will occur.
+<br>
+<h2>Xastir</h2>
+<p>
+    On the FIRST initial run, press the "Save Configs" button in the Settings tab. Xastir will need to be manually started by right-clicking the grey background 
+    in the VNC and selecting it from the menu. Make all your changes to the configuration at this time.
+<br>
+<h2>Misc. Tutorials</h2>
+<a href="extend_fs.php">Extend Filesystem</a> - Extend the filesystem on the SD-card.
