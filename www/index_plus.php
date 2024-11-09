a8d57c517b6f9b5fff149ab5b6a71b71ef042ffe
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-11-08 22:02:19 -0600

    Update index_plus.php

--- www/index_plus.php
+++ www/index_plus.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<LINK href="/styles/simple.css" rel="stylesheet" type="text/css">
 <style>
 body {
   font-family: "Lato", sans-serif;
