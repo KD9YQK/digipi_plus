779c069948a318854a1c37754b85b88b21789066
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-11-10 18:09:05 -0600

    Update index_plus.php

--- www/index_plus.php
+++ www/index_plus.php
@@ -4,73 +4,18 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <LINK href="plus.css" rel="stylesheet" type="text/css">
 <style>
-.body {
-  font-family: "Lato", sans-serif;
-}
-
-.sidebar {
-  height: 100%;
-  width: 0;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  transition: 0.5s;
-  padding-top: 60px;
-}
-
-.sidebar a {
-  padding: 8px 8px 8px 32px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #818181;
-  display: block;
-  transition: 0.3s;
-}
-
-.sidebar a:hover {
-  color: #f1f1f1;
-}
-
-.sidebar .closebtn {
-  position: absolute;
-  top: 0;
-  right: 25px;
-  font-size: 36px;
-  margin-left: 50px;
-}
-
-.openbtn {
-  font-size: 20px;
-  cursor: pointer;
-  background-color: #111;
-  color: white;
-  padding: 10px 15px;
-  left: 1%;
-  bottom: 1%;
-  border: none;
-  position: absolute;
-}
-
-.openbtn:hover {
-  background-color: #444;
-}
-
-.main {
-  transition: margin-left .5s;
-  padding: 16px;
+  .body {
+    font-family: "Lato", sans-serif;
+  }
   
-}
-
-/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
-@media screen and (max-height: 450px) {
-  .sidebar {padding-top: 15px;}
-  .sidebar a {font-size: 18px;}
-}
+  /* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+  @media screen and (max-height: 450px) {
+    .sidebar {padding-top: 15px;}
+    .sidebar a {font-size: 18px;}
+  }
 </style>
 </head>
+
 <body>
 
 <div id="mySidebar" class="sidebar">
@@ -96,7 +41,5 @@
   <iframe src="syslog.php" name="iframe_a" style="height:96vh; width:100%;" title="Digipi Plus" scrolling="no"></iframe>
 </div>
 
-</form>
-</font>
 </body>
 </html>
