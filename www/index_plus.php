4be3f70511431af7205e0772b4a2f8b6f6550934
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-11-11 16:08:57 -0600

    Update index_plus.php

--- www/index_plus.php
+++ www/index_plus.php
@@ -21,73 +21,74 @@
 <div id="mySidebar" class="sidebar">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
   <iframe src="plus_menu.php" name="iframe_b" width=100% height=50% scrolling="vertical"></iframe>
-/* -------------------
-  Menu Start
-  ------------------- */
-<div class="settings">
-<a href="/shell.php" title="Command Prompt" target='iframe_a'>
-  <img src="/images/icons/shell.png" width=32px height=32px>
-  <strong>Shell</strong>
-</a>
+  /* -------------------
+    Menu Start
+    ------------------- */
+  <div class="settings">
+  <a href="/shell.php" title="Command Prompt" target='iframe_a'>
+    <img src="/images/icons/shell.png" width=32px height=32px>
+    <strong>Shell</strong>
+  </a>
+  </div>
+  
+  <div class="settings">
+  <a href="/syslog.php" title="System Log File" target='iframe_a'>
+    <img src="/images/icons/slog.png" width=32px height=32px>
+    <strong>SysLog</strong>
+  </a>
+  </div>
+  
+  <div class="settings">
+  <a href="/log.php" title="Direwolf Packet Log File" target='iframe_a'>
+    <img src="/images/icons/plog.png" width=32px height=32px>
+    <strong>PktLog</strong>
+  </a>
+  </div>
+  
+  <div class="settings">
+  <a href="/help.php" title="Digipi Plus Help" target='iframe_a'>
+    <img src="/images/icons/help.png" width=32px height=32px>
+    <strong>Help</strong>
+  </a>
+  </div>
+  
+  <div class="settings">
+  <script>
+    document.write('<a href="' + window.location.protocol + '//' + window.location.hostname + ':6080/vnc.html?port=6080&password=test11&autoconnect=true" target="iframe_a">' )
+  </script>
+    <img src="/images/icons/screen.png" width=32px height=32px>
+    <strong>VNC</strong>
+  </a>
+  </div>
+  
+  <div class="settings">
+  <script>
+    document.write('<a href="' + window.location.protocol + '//' + window.location.hostname + ':8080" target="iframe_a">' )
+  </script>
+    <img src="/images/icons/email.png" width=32px height=32px>
+    <strong>Email</strong>
+  </a>
+  </div>
+  
+  <div class="settings">
+  <script>
+    document.write('<a href="' + window.location.protocol + '//' + window.location.hostname + ':8055" target="iframe_a">' )
+  </script>
+    <img src="/images/icons/webchat.png" width=32px height=32px>
+    <strong>Webchat</strong>
+  </a>
+  </div>
+  
+  <div class="settings">
+  <a href="/help.php" title="Digipi Plus Help" target='iframe_a'>
+    <img src="/images/icons/help.png" width=32px height=32px>
+    <strong>Help</strong>
+  </a>
+  </div>
+  /* -------------------
+    Menu end
+    ------------------- */
 </div>
-
-<div class="settings">
-<a href="/syslog.php" title="System Log File" target='iframe_a'>
-  <img src="/images/icons/slog.png" width=32px height=32px>
-  <strong>SysLog</strong>
-</a>
-</div>
-
-<div class="settings">
-<a href="/log.php" title="Direwolf Packet Log File" target='iframe_a'>
-  <img src="/images/icons/plog.png" width=32px height=32px>
-  <strong>PktLog</strong>
-</a>
-</div>
-
-<div class="settings">
-<a href="/help.php" title="Digipi Plus Help" target='iframe_a'>
-  <img src="/images/icons/help.png" width=32px height=32px>
-  <strong>Help</strong>
-</a>
-</div>
-
-<div class="settings">
-<script>
-  document.write('<a href="' + window.location.protocol + '//' + window.location.hostname + ':6080/vnc.html?port=6080&password=test11&autoconnect=true" target="iframe_a">' )
-</script>
-  <img src="/images/icons/screen.png" width=32px height=32px>
-  <strong>VNC</strong>
-</a>
-</div>
-
-<div class="settings">
-<script>
-  document.write('<a href="' + window.location.protocol + '//' + window.location.hostname + ':8080" target="iframe_a">' )
-</script>
-  <img src="/images/icons/email.png" width=32px height=32px>
-  <strong>Email</strong>
-</a>
-</div>
-
-<div class="settings">
-<script>
-  document.write('<a href="' + window.location.protocol + '//' + window.location.hostname + ':8055" target="iframe_a">' )
-</script>
-  <img src="/images/icons/webchat.png" width=32px height=32px>
-  <strong>Webchat</strong>
-</a>
-</div>
-
-<div class="settings">
-<a href="/help.php" title="Digipi Plus Help" target='iframe_a'>
-  <img src="/images/icons/help.png" width=32px height=32px>
-  <strong>Help</strong>
-</a>
-</div>
-/* -------------------
-  Menu end
-  ------------------- */
 <div id="main">
   <button class="openbtn" onclick="openNav()">☰</button>  
   <iframe src="home_plus.php" name="iframe_a" style="height:96vh; width:100%;" title="Digipi Plus" scrolling="no"></iframe>
