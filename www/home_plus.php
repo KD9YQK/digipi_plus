090b06ae990e435d6c096a6b72ec0a9bd66f3e36
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-11-10 19:52:34 -0600

    Update home_plus.php

--- www/home_plus.php
+++ www/home_plus.php
@@ -2,11 +2,13 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<LINK href="/styles/simple.css" rel="stylesheet" type="text/css">
+<LINK href="/styles/plus.css" rel="stylesheet" type="text/css">
 </head>
 <body>
   <div>
+    <center>
     <h1>DigiPi Plus</h1>
+    </center>
   </div>
 </body>
 </html>
