056b719a485d272673f0b25facea1a6d009cad6f
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-11-11 16:32:48 -0600

    Update plus_settings.php

--- www/plus_settings.php
+++ www/plus_settings.php
@@ -1,46 +1,46 @@
-<div class="settings">
+<div class="settings" style="margin:0 0 1.5em;">
 <a href="/wifi.php" title="Setup Wifi connection">
   <img src="/images/icons/wifi.png" width=32px height=32px>
   <strong>Wifi</strong>
 </a>
 </div>
 
-<div class="settings">
+<div class="settings" style="margin:0 0 1.5em;">
 <a href="/shell.php" title="Command Prompt" target="shell">
   <img src="/images/icons/shell.png" width=32px height=32px>
   <strong>Shell</strong>
 </a>
 </div>
 
-<div class="settings">
+<div class="settings" style="margin:0 0 1.5em;">
 <a href="/syslog.php" title="System Log File" target="syslog">
   <img src="/images/icons/slog.png" width=32px height=32px>
   <strong>SysLog</strong>
 </a>
 </div>
 
-<div class="settings">
+<div class="settings" style="margin:0 0 1.5em;">
 <a href="/log.php" title="Direwolf Packet Log File" target="pktlog">
   <img src="/images/icons/plog.png" width=32px height=32px>
   <strong>PktLog</strong>
 </a>
 </div>
 
-<div class="settings">
+<div class="settings" style="margin:0 0 1.5em;">
 <a href="/direwatch.php" title="Direwatch Screen" target="screen">
   <img src="/images/icons/screen.png" width=32px height=32px>
   <strong>Screen</strong>
 </a>
 </div>
 
-<div class="settings">
+<div class="settings" style="margin:0 0 1.5em;">
 <a href="/index.php" title="Refresh">
   <img src="/images/icons/refresh.png" width=32px height=32px>
   <strong>Refresh</strong>
 </a>
 </div>
 
-<div class="settings">
+<div class="settings" style="margin:0 0 1.5em;">
 <a href="/help.php" title="Digipi Plus Help" target="help">
   <img src="/images/icons/help.png" width=32px height=32px>
   <strong>Help</strong>
