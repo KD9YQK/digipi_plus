06e833c18fa3b030445259b8a28aa4d279b968bd
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-11-11 18:50:02 -0600

    Update home_plus.php

--- www/home_plus.php
+++ www/home_plus.php
@@ -8,7 +8,7 @@
   <div style="margin-top:20%">
     <center>
     <h1 style="font-size:48px;">DigiPi Plus</h1>
-    <div style="margin-bottom:20%;">
+    <div style="margin-top:80%;">
       <p>DigiPi is created by <a href="https://digipi.org" target="_blank">Craig KM6LYW</a></p>
       <p>DigiPi Plus is created by <a href="https://kd9yqk.com" target="_blank">Bob KD9YQK</a></p>
     </div>
