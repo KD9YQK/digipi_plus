21e2ccc798128403917418febf9e3d6da0bae975
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-11-13 16:08:31 -0600

    Update direwolf.rtlsdr.sh

--- launchers/direwolf.rtlsdr.sh
+++ launchers/direwolf.rtlsdr.sh
@@ -21,17 +21,20 @@ rtl_fm -f 144.390 - | direwolf -d i -p -q d -t 0 -D 1 -r 24000 -c /run/direwolf.
 
 sleep 1 # wait for direwolf to initialize gpio
 
-source <(head -n 25 localize.sh)
+source <(head -n 25 localize.env)
 
 # remove "-o" flag to see list of stations
 if [ $NEWLAT = 39.9999 ]; then  # location not yet set
-   sudo /home/pi/direwatch.py -o --save "/run/direwatch.png" --log "/run/direwolf.log" --title_text "RTL-SDR iGate"  &
+   sudo /home/pi/direwatch.py -o --save "/run/direwatch.png" --log "/run/direwolf.log" --title_text "DigiPi TNC" --display $NEWDISPLAYTYPE  &
 else
-   sudo /home/pi/direwatch.py -o --save "/run/direwatch.png" --log "/run/direwolf.log" --title_text "RTL-SDR iGate" --lat $NEWLAT --lon $NEWLON  &
+   sudo /home/pi/direwatch.py -o --save "/run/direwatch.png" --log "/run/direwolf.log" --title_text "DigiPi TNC" --lat $NEWLAT --lon $NEWLON --display $NEWDISPLAYTYPE &
 fi
 
 # wait for direwolf to open port 8001
 sleep 1
+
+# bind bluetooth serial port to direwolf's KISS interface on port 8001
+sudo rfcomm --raw watch /dev/rfcomm0 1 socat -d -d tcp4:127.0.0.1:8001 /dev/rfcomm0  > /tmp/rfcom.out 2>/tmp/rfcom.out
 
 sleep infinity
 
