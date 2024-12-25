f65f3c419eca983d2453f62f829bd6c2271dca4c
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-12-25 11:18:54 -0600

    Update plus_menu.php

--- www/plus_menu.php
+++ www/plus_menu.php
@@ -3,6 +3,7 @@
 <LINK href="/styles/plus.css" rel="stylesheet" type="text/css">
 <script>
 function openMenu(evt, tabName) {
+    echo tabName;
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
