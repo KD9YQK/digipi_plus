9a0873638cb09a32f55b2d7f8b0bad178d22e004
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-12-26 13:47:51 -0600

    Update mmbr_helper.py

--- mmbr_helper.py
+++ mmbr_helper.py
@@ -5,7 +5,7 @@ with open("www/plus_links.php") as f_old, open("temp/plus_links.php", "w") as f_
     for line in f_old:
         f_new.write(line)
         if "<!-- Row2 -->" in line:
-            f_new.write("""  <td width="100px"><script language="JavaScript">document.write('<a href="' + window.location.protocol + '//' + window.location.hostname + ':8073' + '" target="mmbr" title="MMBR - Messages Must Be Received"><strong>MMBR</strong></a> ' );</script></td>\n""")
+            f_new.write("""  <td width="100px"><script language="JavaScript">document.write('<a href="' + window.location.protocol + '//' + window.location.hostname + ':5000' + '" target="mmbr" title="MMBR - Messages Must Be Received"><strong>MMBR</strong></a> ' );</script></td>\n""")
 
 
 
