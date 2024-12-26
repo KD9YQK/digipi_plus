b2c753b8cedf3b84873d830f5a0e34191726354e
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-12-26 13:29:02 -0600

    Update mmbr_helper.py

--- mmbr_helper.py
+++ mmbr_helper.py
@@ -5,7 +5,7 @@ with open("www/plus_links.php") as f_old, open("temp/plus_links.php", "w") as f_
     for line in f_old:
         f_new.write(line)
         if "<!-- Row2 -->" in line:
-            f_new.write("  <td><a href='127.0.0.1:5000' target='mmbr' title='Display MMBR Screen'><strong>MMBR</strong></a></td>\n")
+            f_new.write("  <td><a href='http://127.0.0.1:5000' target='mmbr' title='Display MMBR Screen'><strong>MMBR</strong></a></td>\n")
 
 
 # Press the green button in the gutter to run the script.
