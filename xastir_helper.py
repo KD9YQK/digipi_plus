edeea91716752409121cfaaaf18282baee34ac07
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-26 00:48:27 -0600

    Update xastir_helper.py

--- xastir_helper.py
+++ xastir_helper.py
@@ -1,5 +1,5 @@
 #!/bin/python
-
+from helper import add_service_menu
 from helper import add_EXECSTARTPRE
 
 
@@ -29,26 +29,8 @@ with open("www/plus_form.php") as f_old, open("temp/plus_form.php", "w") as f_ne
         f_new.write(line)
 
         if "<?php" in line:
-            f_new.write("#-- PCSI  -------------------------------------------------\n\n"
-                        "echo '<tr>';\n"
-                        "$output = shell_exec('systemctl is-active xastir');\n"
-                        "#$output = str_replace('failed', 'inactive', $output);\n"
-                        "$output = chop($output);\n"
-                        "if ($output == 'active'){\n"
-                        """  echo '<td bgcolor="lightgreen">';\n"""
-                        "}\n"
-                        "elseif ($output == 'failed'){\n"
-                        """  echo '<td bgcolor="lightgreen">';}\n"""
-                        "else{\n"
-                        """  echo '<td bgcolor="lightgrey">';}\n"""
-                        "echo '&nbsp;';\n"
-                        "echo '</td><td>';\n"
-                        "echo '<font size=+1>Xastir</font></td>';\n"
-                        """echo '<td align="right" nowrap>';\n"""
-                        """echo '<input type="submit" name="xastir" value="on"> ';\n"""
-                        """echo '<input type="submit" name="xastir" value="off">';\n"""
-                        "echo '</font>';\n"
-                        "echo '</td></tr>';\n")
+            asm = add_service_menu("Xastir", "xastir", "xastir")
+            f_new.write(asm)
 
 
 with open("www/plus_links.php") as f_old, open("temp/plus_links.php", "w") as f_new:
