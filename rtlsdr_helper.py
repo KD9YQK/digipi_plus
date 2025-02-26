cc593d7df0a10bba3c3a6f96fa5d19b2899c125e
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-26 00:42:30 -0600

    Update rtlsdr_helper.py

--- rtlsdr_helper.py
+++ rtlsdr_helper.py
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 from helper import add_EXECSTARTPRE
+from helper import add_service_menu
 
 
 with open("/home/pi/direwolf.tnc.conf") as f_old, open("/home/pi/digipi_plus/launchers/direwolf.rtlsdr.conf", "w") as f_new:
@@ -43,39 +44,8 @@ with open("www/plus_form.php") as f_old, open("temp/plus_form.php", "w") as f_ne
         f_new.write(line)
 
         if "<?php" in line:
-            f_new.write("#-- RTLSDR IGATE  -------------------------------------------------\n\n"
-                        echo "<tr>";\n
-                        $output = shell_exec('systemctl is-active rtlsdr-igate');\n
-                        $output = chop($output);\n
-                          if ($output == "active")\n
-                          {\n
-                             echo '<td bgcolor="lightgreen">';\n
-                             $checked = "checked";\n
-                          }\n
-                          elseif ($output == "failed")\n
-                          {\n
-                             echo '<td bgcolor="red">';\n
-                             $checked = "";\n
-                          }\n
-                          else\n
-                          {\n
-                             echo '<td bgcolor="lightgrey">';\n
-                             $checked = "";\n
-                          }
-                        echo '</td>';\n
-                        echo '<td>';\n
-                        echo '<font size=+1>RTL-SDR igate</font></td>';\n
-                        echo '<td nowrap>';\n
-                        echo '<form action="plus_menu.php" method="post">';\n
-                        echo '<label class="switch switch-light">';\n
-                        echo '  <input type="hidden" name="rtlsdr-igate" value="off">';\n
-                        echo "  <input onChange='this.form.submit()' class='switch-input' type='checkbox' name='rtlsdr-igate' value='on'  $checked />";\n
-                        echo '  <span class="switch-label" ></span> ';\n
-                        echo '  <span class="switch-handle"></span> ';\n
-                        echo '</label>';\n
-                        echo '</form>';\n
-                        echo '</font>';\n
-                        echo '</td></tr>';\n)
+            asm = add_service_menu("RTL-SDR igate", "rtlsdr-igate", "rtlsdr-igate")
+            f_new.write(asm)
 
 
 # Press the green button in the gutter to run the script.
