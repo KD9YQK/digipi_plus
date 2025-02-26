b43b06ead21ffcced4714d0c20aa0b2633a3671d
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-26 00:40:29 -0600

    Update pcsi_helper.py

--- pcsi_helper.py
+++ pcsi_helper.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 from helper import add_EXECSTARTPRE
+from helper import add_service_menu
 
 
 with open("www/plus_services.php") as f_old, open("temp/plus_services.php", "w") as f_new:
@@ -29,39 +30,8 @@ with open("www/plus_form.php") as f_old, open("temp/plus_form.php", "w") as f_ne
         f_new.write(line)
 
         if "<?php" in line:
-            f_new.write("#-- PCSI  -------------------------------------------------\n\n"
-                        echo "<tr>";\n
-                        $output = shell_exec('systemctl is-active pcsi');\n
-                        $output = chop($output);\n
-                          if ($output == "active")\n
-                          {\n
-                             echo '<td bgcolor="lightgreen">';\n
-                             $checked = "checked";\n
-                          }\n
-                          elseif ($output == "failed")\n
-                          {\n
-                             echo '<td bgcolor="red">';\n
-                             $checked = "";\n
-                          }\n
-                          else\n
-                          {\n
-                             echo '<td bgcolor="lightgrey">';\n
-                             $checked = "";\n
-                          }
-                        echo '</td>';\n
-                        echo '<td>';\n
-                        echo '<font size=+1>PCSI</font></td>';\n
-                        echo '<td nowrap>';\n
-                        echo '<form action="plus_menu.php" method="post">';\n
-                        echo '<label class="switch switch-light">';\n
-                        echo '  <input type="hidden" name="pcsi" value="off">';\n
-                        echo "  <input onChange='this.form.submit()' class='switch-input' type='checkbox' name='pcsi' value='on'  $checked />";\n
-                        echo '  <span class="switch-label" ></span> ';\n
-                        echo '  <span class="switch-handle"></span> ';\n
-                        echo '</label>';\n
-                        echo '</form>';\n
-                        echo '</font>';\n
-                        echo '</td></tr>';\n)
+            asm = add_service_menu("PCSI", "pcsi", "pcsi")
+            f_new.write(asm)
 
 
 with open("www/plus_links.php") as f_old, open("temp/plus_links.php", "w") as f_new:
