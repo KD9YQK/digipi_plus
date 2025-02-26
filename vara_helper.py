fcd7731aa5d6e4659e6413707ebaa2fa63570f0f
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-25 18:59:04 -0600

    Create vara_helper.py

---
+++ vara_helper.py
@@ -0,0 +1,99 @@
+#!/bin/python
+
+from helper import add_EXECSTARTPRE
+
+
+with open("www/plus_services.php") as f_old, open("temp/plus_services.php", "w") as f_new:
+    for line in f_old:
+        if "?>" in line:
+            f_new.write("if (isset($_POST['varahf'])) {\n"
+                        "  $submit = $_POST['varahf'];\n"
+                        "  if ( $submit == 'on' ) {\n"
+                        "    $output = shell_exec('sudo systemctl start varahf');\n"
+                        "    sleep(5);\n"
+                        "    echo $output;\n"
+                        "  }\n"
+                        "  if ( $submit == 'off' ) {\n"
+                        "    $output = shell_exec('sudo systemctl stop varahf');\n"
+                        "    echo $output;\n"
+                        "  }\n"
+                        "}\n")
+            f_new.write("if (isset($_POST['varafm'])) {\n"
+                        "  $submit = $_POST['varafm'];\n"
+                        "  if ( $submit == 'on' ) {\n"
+                        "    $output = shell_exec('sudo systemctl start varafm');\n"
+                        "    sleep(5);\n"
+                        "    echo $output;\n"
+                        "  }\n"
+                        "  if ( $submit == 'off' ) {\n"
+                        "    $output = shell_exec('sudo systemctl stop varafm');\n"
+                        "    echo $output;\n"
+                        "  }\n"
+                        "}\n")
+        f_new.write(line)
+
+
+with open("www/plus_form.php") as f_old, open("temp/plus_form.php", "w") as f_new:
+    for line in f_old:
+        if "?>" in line:
+            f_new.write("$output = shell_exec('sudo systemctl reset-failed varahf 2> /dev/null');\n")
+            f_new.write("$output = shell_exec('sudo systemctl reset-failed varafm 2> /dev/null');\n")
+    
+        f_new.write(line)
+
+        if "<?php" in line:
+            f_new.write("#-- VaraHF  -------------------------------------------------\n\n"
+                        "echo '<tr>';\n"
+                        "$output = shell_exec('systemctl is-active varahf');\n"
+                        "#$output = str_replace('failed', 'inactive', $output);\n"
+                        "$output = chop($output);\n"
+                        "if ($output == 'active'){\n"
+                        """  echo '<td bgcolor="lightgreen">';\n"""
+                        "}\n"
+                        "elseif ($output == 'failed'){\n"
+                        """  echo '<td bgcolor="lightgreen">';}\n"""
+                        "else{\n"
+                        """  echo '<td bgcolor="lightgrey">';}\n"""
+                        "echo '&nbsp;';\n"
+                        "echo '</td><td>';\n"
+                        "echo '<font size=+1>VaraHF</font></td>';\n"
+                        """echo '<td align="right" nowrap>';\n"""
+                        """echo '<input type="submit" name="varahf" value="on"> ';\n"""
+                        """echo '<input type="submit" name="varahf" value="off">';\n"""
+                        "echo '</font>';\n"
+                        "echo '</td></tr>';\n")
+          f_new.write("#-- VaraFM  -------------------------------------------------\n\n"
+                        "echo '<tr>';\n"
+                        "$output = shell_exec('systemctl is-active varafm');\n"
+                        "#$output = str_replace('failed', 'inactive', $output);\n"
+                        "$output = chop($output);\n"
+                        "if ($output == 'active'){\n"
+                        """  echo '<td bgcolor="lightgreen">';\n"""
+                        "}\n"
+                        "elseif ($output == 'failed'){\n"
+                        """  echo '<td bgcolor="lightgreen">';}\n"""
+                        "else{\n"
+                        """  echo '<td bgcolor="lightgrey">';}\n"""
+                        "echo '&nbsp;';\n"
+                        "echo '</td><td>';\n"
+                        "echo '<font size=+1>VaraFM</font></td>';\n"
+                        """echo '<td align="right" nowrap>';\n"""
+                        """echo '<input type="submit" name="varafm" value="on"> ';\n"""
+                        """echo '<input type="submit" name="varafm" value="off">';\n"""
+                        "echo '</font>';\n"
+                        "echo '</td></tr>';\n")
+          
+
+
+with open("www/plus_links.php") as f_old, open("temp/plus_links.php", "w") as f_new:
+    script_count = 0
+    for line in f_old:
+        f_new.write(line)
+        if "<!-- Row3 -->" in line:
+            f_new.write("  <td><a href='/vara' target='vara' title='Display VaraHF screen'><strong>VaraHF</strong></a></td>\n")
+            f_new.write("  <td><a href='/vara' target='vara' title='Display VaraFM screen'><strong>VaraFM</strong></a></td>\n")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
