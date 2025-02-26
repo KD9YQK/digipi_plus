4bdd33f7fa2bb98ad6b17cda45e55850e5d2c4f7
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-26 01:53:49 -0600

    Update openwebrx_helper.py

--- openwebrx_helper.py
+++ openwebrx_helper.py
@@ -1,27 +1,16 @@
 #!/bin/python
-from helper import add_service_menu
+from helper import add_service add_form
 
 
 with open("www/plus_services.php") as f_old, open("temp/plus_services.php", "w") as f_new:
     for line in f_old:
         if "?>" in line:
-            f_new.write("if (isset($_POST['openwebrx'])) {\n"
-                        "  $submit = $_POST['openwebrx'];\n"
-                        "  if ( $submit == 'on' ) {\n"
-                        "    $output = shell_exec('sudo systemctl start openwebrx-digipi');\n"
-                        "    sleep(5);\n"
-                        "    echo $output;\n"
-                        "  }\n"
-                        "  if ( $submit == 'off' ) {\n"
-                        "    $output = shell_exec('sudo systemctl stop openwebrx-digipi');\n"
-                        "    echo $output;\n"
-                        "  }\n"
-                        "}\n")
+            asv = add_service("openwebrx-digipi", "openwebrx")
+            f_new.write(asv)
         f_new.write(line)
 
 
 with open("www/plus_form.php") as f_old, open("temp/plus_form.php", "w") as f_new:
-    asm = add_service_menu("OpenWebRX+", "openwebrx-digipi", "openwebrx")
     for line in f_old:
         if "?>" in line:
             f_new.write("$output = shell_exec('sudo systemctl reset-failed openwebrx-digipi 2> /dev/null');\n")
@@ -29,6 +18,7 @@ with open("www/plus_form.php") as f_old, open("temp/plus_form.php", "w") as f_ne
         f_new.write(line)
 
         if "<?php" in line:
+            asm = add_form("OpenWebRX+", "openwebrx-digipi", "openwebrx")
             f_new.write(asm)
 
 
