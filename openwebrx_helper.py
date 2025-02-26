a18e4a128288dfe8ee9f6dd8c3cbb81a7519d8f9
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-25 20:52:59 -0600

    Update openwebrx_helper.py

--- openwebrx_helper.py
+++ openwebrx_helper.py
@@ -27,38 +27,38 @@ with open("www/plus_form.php") as f_old, open("temp/plus_form.php", "w") as f_ne
 
         if "<?php" in line:
             f_new.write("#-- OpenWebRX  -------------------------------------------------\n\n"
-                        echo "<tr>";\n
-                        $output = shell_exec('systemctl is-active openwebrx-digipi');\n
-                        $output = chop($output);\n
-                          if ($output == "active")\n
-                          {\n
-                             echo '<td bgcolor="lightgreen">';\n
-                             $checked = "checked";\n
-                          }\n
-                          elseif ($output == "failed")\n
-                          {\n
-                             echo '<td bgcolor="red">';\n
-                             $checked = "";\n
-                          }\n
-                          else\n
-                          {\n
-                             echo '<td bgcolor="lightgrey">';\n
-                             $checked = "";\n
-                          }
-                        echo '</td>';\n
-                        echo '<td>';\n
-                        echo '<font size=+1>OpenWebRX+</font></td>';\n
-                        echo '<td nowrap>';\n
-                        echo '<form action="plus_menu.php" method="post">';\n
-                        echo '<label class="switch switch-light">';\n
-                        echo '  <input type="hidden" name="openwebrx" value="off">';\n
-                        echo "  <input onChange='this.form.submit()' class='switch-input' type='checkbox' name='openwebrx' value='on'  $checked />";\n
-                        echo '  <span class="switch-label" ></span> ';\n
-                        echo '  <span class="switch-handle"></span> ';\n
-                        echo '</label>';\n
-                        echo '</form>';\n
-                        echo '</font>';\n
-                        echo '</td></tr>';\n)
+                        "echo '<tr>';\n"
+                        "$output = shell_exec('systemctl is-active openwebrx-digipi');\n"
+                        "$output = chop($output);\n"
+                         " if ($output == 'active')\n"
+                         " {\n"
+                         """    echo '<td bgcolor="lightgreen">';\n"""
+                         "    $checked = 'checked';\n"
+                            "}\n"
+                          "elseif ($output == 'failed')\n"
+                          "{\n"
+                          """   echo '<td bgcolor="red">';\n"""
+                          "   $checked = '';\n"
+                          "}\n"
+                          "else\n"
+                          "{\n"
+                          """   echo '<td bgcolor="lightgrey">';\n"""
+                          "   $checked = '';\n"
+                          "}\n"
+                        "echo '</td>';\n"
+                        "echo '<td>';\n"
+                        "echo '<font size=+1>OpenWebRX+</font></td>';\n"
+                        "echo '<td nowrap>';\n"
+                        """echo '<form action="plus_menu.php" method="post">';\n"""
+                        """echo '<label class="switch switch-light">';\n"""
+                        """echo '  <input type="hidden" name="openwebrx" value="off">';\n"""
+                        """echo "  <input onChange='this.form.submit()' class='switch-input' type='checkbox' name='openwebrx' value='on'  $checked />";\n"""
+                        """echo '  <span class="switch-label" ></span> ';\n"""
+                        """echo '  <span class="switch-handle"></span> ';\n"""
+                        "echo '</label>';\n"
+                        "echo '</form>';\n"
+                        "echo '</font>';\n"
+                        "echo '</td></tr>';\n")
 
 
 with open("www/plus_links.php") as f_old, open("temp/plus_links.php", "w") as f_new:
