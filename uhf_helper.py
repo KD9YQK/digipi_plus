fa218780949237d41ad3b6e44c1e2cb2eb891247
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-26 00:45:07 -0600

    Update uhf_helper.py

--- uhf_helper.py
+++ uhf_helper.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 from helper import add_EXECSTARTPRE
+from helper import add_service_menu
 
 
 with open("www/plus_services.php") as f_old, open("temp/plus_services.php", "w") as f_new:
@@ -42,72 +43,10 @@ with open("www/plus_form.php") as f_old, open("temp/plus_form.php", "w") as f_ne
         f_new.write(line)
 
         if "<?php" in line:
-            f_new.write("#-- Direwolf UHF TNC  -------------------------------------------------\n\n"
-                        echo "<tr>";\n
-                        $output = shell_exec('systemctl is-active uhf-tnc');\n
-                        $output = chop($output);\n
-                          if ($output == "active")\n
-                          {\n
-                             echo '<td bgcolor="lightgreen">';\n
-                             $checked = "checked";\n
-                          }\n
-                          elseif ($output == "failed")\n
-                          {\n
-                             echo '<td bgcolor="red">';\n
-                             $checked = "";\n
-                          }\n
-                          else\n
-                          {\n
-                             echo '<td bgcolor="lightgrey">';\n
-                             $checked = "";\n
-                          }
-                        echo '</td>';\n
-                        echo '<td>';\n
-                        echo '<font size=+1>UHF TNC/igate</font></td>';\n
-                        echo '<td nowrap>';\n
-                        echo '<form action="plus_menu.php" method="post">';\n
-                        echo '<label class="switch switch-light">';\n
-                        echo '  <input type="hidden" name="uhftnc" value="off">';\n
-                        echo "  <input onChange='this.form.submit()' class='switch-input' type='checkbox' name='uhftnc' value='on'  $checked />";\n
-                        echo '  <span class="switch-label" ></span> ';\n
-                        echo '  <span class="switch-handle"></span> ';\n
-                        echo '</label>';\n
-                        echo '</form>';\n
-                        echo '</font>';\n
-                        echo '</td></tr>';\n)
-            f_new.write("#-- Direwolf UHF NODE  -------------------------------------------------\n\n"
-                        echo "<tr>";\n
-                        $output = shell_exec('systemctl is-active uhf-node');\n
-                        $output = chop($output);\n
-                          if ($output == "active")\n
-                          {\n
-                             echo '<td bgcolor="lightgreen">';\n
-                             $checked = "checked";\n
-                          }\n
-                          elseif ($output == "failed")\n
-                          {\n
-                             echo '<td bgcolor="red">';\n
-                             $checked = "";\n
-                          }\n
-                          else\n
-                          {\n
-                             echo '<td bgcolor="lightgrey">';\n
-                             $checked = "";\n
-                          }
-                        echo '</td>';\n
-                        echo '<td>';\n
-                        echo '<font size=+1>UHF Node</font></td>';\n
-                        echo '<td nowrap>';\n
-                        echo '<form action="plus_menu.php" method="post">';\n
-                        echo '<label class="switch switch-light">';\n
-                        echo '  <input type="hidden" name="uhfnode" value="off">';\n
-                        echo "  <input onChange='this.form.submit()' class='switch-input' type='checkbox' name='uhfnode' value='on'  $checked />";\n
-                        echo '  <span class="switch-label" ></span> ';\n
-                        echo '  <span class="switch-handle"></span> ';\n
-                        echo '</label>';\n
-                        echo '</form>';\n
-                        echo '</font>';\n
-                        echo '</td></tr>';\n)
+            asm = add_service_menu("Direwolf UHF TNC", "uhf-tnc", "uhftnc")
+            f_new.write(asm)
+            asm = add_service_menu("Direwolf UHF TNC", "uhf-node", "uhfnode")
+            f_new.write(asm)
 
 
 # Press the green button in the gutter to run the script.
