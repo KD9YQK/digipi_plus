487aa92348d4edc827260f9ffaa1dfbe5d95d759
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-26 02:08:44 -0600

    Update openwebrx_helper.py

--- openwebrx_helper.py
+++ openwebrx_helper.py
@@ -1,5 +1,6 @@
 #!/bin/python
-from helper import add_service add_form
+from helper import add_service
+from helper import add_form
 
 
 with open("www/plus_services.php") as f_old, open("temp/plus_services.php", "w") as f_new:
