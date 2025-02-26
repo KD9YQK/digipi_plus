61f14dd1aeb323cd9985eb32a7519cc943ac7718
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-25 20:37:14 -0600

    Update vara_helper.py

--- vara_helper.py
+++ vara_helper.py
@@ -43,45 +43,71 @@ with open("www/plus_form.php") as f_old, open("temp/plus_form.php", "w") as f_ne
 
         if "<?php" in line:
             f_new.write("#-- VaraHF  -------------------------------------------------\n\n"
-                        "echo '<tr>';\n"
-                        "$output = shell_exec('systemctl is-active varahf');\n"
-                        "#$output = str_replace('failed', 'inactive', $output);\n"
-                        "$output = chop($output);\n"
-                        "if ($output == 'active'){\n"
-                        """  echo '<td bgcolor="lightgreen">';\n"""
-                        "}\n"
-                        "elseif ($output == 'failed'){\n"
-                        """  echo '<td bgcolor="lightgreen">';}\n"""
-                        "else{\n"
-                        """  echo '<td bgcolor="lightgrey">';}\n"""
-                        "echo '&nbsp;';\n"
-                        "echo '</td><td>';\n"
-                        "echo '<font size=+1>VaraHF</font></td>';\n"
-                        """echo '<td align="right" nowrap>';\n"""
-                        """echo '<input type="submit" name="varahf" value="on"> ';\n"""
-                        """echo '<input type="submit" name="varahf" value="off">';\n"""
-                        "echo '</font>';\n"
-                        "echo '</td></tr>';\n")
+                        echo "<tr>";\n
+                        $output = shell_exec('systemctl is-active varahf');\n
+                        $output = chop($output);\n
+                          if ($output == "active")\n
+                          {\n
+                             echo '<td bgcolor="lightgreen">';\n
+                             $checked = "checked";\n
+                          }\n
+                          elseif ($output == "failed")\n
+                          {\n
+                             echo '<td bgcolor="red">';\n
+                             $checked = "";\n
+                          }\n
+                          else\n
+                          {\n
+                             echo '<td bgcolor="lightgrey">';\n
+                             $checked = "";\n
+                          }
+                        echo '</td>';\n
+                        echo '<td>';\n
+                        echo '<font size=+1>Vara HF</font></td>';\n
+                        echo '<td nowrap>';\n
+                        echo '<form action="plus_menu.php" method="post">';\n
+                        echo '<label class="switch switch-light">';\n
+                        echo '  <input type="hidden" name="varahf" value="off">';\n
+                        echo "  <input onChange='this.form.submit()' class='switch-input' type='checkbox' name='varahf' value='on'  $checked />";\n
+                        echo '  <span class="switch-label" ></span> ';\n
+                        echo '  <span class="switch-handle"></span> ';\n
+                        echo '</label>';\n
+                        echo '</form>';\n
+                        echo '</font>';\n
+                        echo '</td></tr>';\n)
             f_new.write("#-- VaraFM  -------------------------------------------------\n\n"
-                        "echo '<tr>';\n"
-                        "$output = shell_exec('systemctl is-active varafm');\n"
-                        "#$output = str_replace('failed', 'inactive', $output);\n"
-                        "$output = chop($output);\n"
-                        "if ($output == 'active'){\n"
-                        """  echo '<td bgcolor="lightgreen">';\n"""
-                        "}\n"
-                        "elseif ($output == 'failed'){\n"
-                        """  echo '<td bgcolor="lightgreen">';}\n"""
-                        "else{\n"
-                        """  echo '<td bgcolor="lightgrey">';}\n"""
-                        "echo '&nbsp;';\n"
-                        "echo '</td><td>';\n"
-                        "echo '<font size=+1>VaraFM</font></td>';\n"
-                        """echo '<td align="right" nowrap>';\n"""
-                        """echo '<input type="submit" name="varafm" value="on"> ';\n"""
-                        """echo '<input type="submit" name="varafm" value="off">';\n"""
-                        "echo '</font>';\n"
-                        "echo '</td></tr>';\n")
+                        echo "<tr>";\n
+                        $output = shell_exec('systemctl is-active varafm');\n
+                        $output = chop($output);\n
+                          if ($output == "active")\n
+                          {\n
+                             echo '<td bgcolor="lightgreen">';\n
+                             $checked = "checked";\n
+                          }\n
+                          elseif ($output == "failed")\n
+                          {\n
+                             echo '<td bgcolor="red">';\n
+                             $checked = "";\n
+                          }\n
+                          else\n
+                          {\n
+                             echo '<td bgcolor="lightgrey">';\n
+                             $checked = "";\n
+                          }
+                        echo '</td>';\n
+                        echo '<td>';\n
+                        echo '<font size=+1>Vara FM</font></td>';\n
+                        echo '<td nowrap>';\n
+                        echo '<form action="plus_menu.php" method="post">';\n
+                        echo '<label class="switch switch-light">';\n
+                        echo '  <input type="hidden" name="varafm" value="off">';\n
+                        echo "  <input onChange='this.form.submit()' class='switch-input' type='checkbox' name='varafm' value='on'  $checked />";\n
+                        echo '  <span class="switch-label" ></span> ';\n
+                        echo '  <span class="switch-handle"></span> ';\n
+                        echo '</label>';\n
+                        echo '</form>';\n
+                        echo '</font>';\n
+                        echo '</td></tr>';\n)
           
 
 
