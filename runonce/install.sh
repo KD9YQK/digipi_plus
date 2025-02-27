4a628a3d14233e349c66dc6e8a57b8f62325616f
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-02-26 19:49:36 -0600

    Update install.sh

--- runonce/install.sh
+++ runonce/install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Place any script you want run at the next reboot (once only) in the directory /etc/local/runonce.d and chown and chmod +x it appropriately. 
+# Once it's been run, you'll find it moved to the ran subdirectory and the date and time appended to its name. 
+# There will also be an entry in your syslog.
+
 sudo mkdir -p /etc/local/runonce.d/ran
 sudo cp runonce /usr/local/bin/runonce
 sudo chmod +x /usr/local/bin/runonce
@@ -7,7 +11,13 @@ sudo chmod +x /usr/local/bin/runonce
 #sudo systemctl start runonce.service
 
 #write out current crontab
-crontab -l > mycron
+test=$(crontab -l)
+if [ -z $test ] ; then
+  echo "@reboot /bin/bash /usr/local/bin/runonce" > mycron
+else
+  crontab -l > mycron
+  echo "" >> mycron
+  echo "@reboot /bin/bash /usr/local/bin/runonce" >> mycron
 #echo new cron into cron file
 echo "@reboot /bin/bash /usr/local/bin/runonce" >> mycron
 #install new cron file
